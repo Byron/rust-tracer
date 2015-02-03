b350c6c8857f4b1b72aabe480cc557f0dfa0991f
Sebastian Thiel <byronimo@gmail.com>
2015-02-03 07:36:38 +0100

    Fixed normalize() implementation.
    Including test-case.
    Cleaned up module layout.

--- src/rust/vec.rs
+++ src/rust/vec.rs
@@ -1,8 +1,6 @@
 //! A module implementing a Vector type which can be parametized to support different
 //! floating point precision.
 
-extern crate test;
-
 use std::num::Float;
 use std::ops::{Add, Sub, Mul};
 
@@ -73,62 +71,78 @@ impl<'a, T: Float> Vector<T> {
         self.x * r.x + self.y * r.y + self.z * r.z
     }
 
-    // #[inline(always)]
-    // fn normalize(&'a mut self) -> &'a mut Vector<T> {
-    //     self.mulf(Float::one() / self.dot(self).sqrt())
-    // }
-    // fn normalized(&self) -> Vector<T> {
-    //     self.mulfed(Float::one() as T / self.dot(self).sqrt())
-    // }
+    #[inline(always)]
+    fn len(&self) -> T {
+        self.dot(self).sqrt()
+    }
+
+    #[allow(unstable)]
+    #[inline(always)]
+    fn normalize(&'a mut self) -> &'a mut Vector<T> {
+        let len = self.len();
+        self.mulf(<T as Float>::one() / len)
+    }
+
+    #[allow(unstable)]
+    #[inline(always)]
+    fn normalized(&self) -> Vector<T> {
+        self.mulfed(<T as Float>::one() / self.len())
+    }
 }
 
 
 #[cfg(test)]
-#[test]
-fn basics() {
-    let v32 = Vector { x: 5.0f32, y: 4.0f32, z: 0.0f32 };
-    assert_eq!(v32.x, 5.0f32);
-    assert_eq!(v32, v32);
-    assert!(!(v32 != v32));
-    {
-        // This is a copyable type - if not, this assignment would fail
-        let mut copy = v32;
-        copy.x = 10.0f32;
+mod tests {
+    extern crate test;
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let v32 = Vector { x: 5.0f32, y: 4.0f32, z: 0.0f32 };
+        assert_eq!(v32.x, 5.0f32);
+        assert_eq!(v32, v32);
+        assert!(!(v32 != v32));
+        {
+            // This is a copyable type - if not, this assignment would fail
+            let mut copy = v32;
+            copy.x = 10.0f32;
+        }
+
+        let v64 = Vector { x: 1.0f64, y: 2.0f64, z: 3.0f64, };
+        assert_eq!(v64.x, 1.0f64);
+
+        println!("{:?}", v32);
+        println!("{:?}", v64);
+
+        // Addition
+        let v = v32 + v32;
+        assert_eq!(v.x, v32.x + v32.x);
+        assert_eq!(v.y, v32.y + v32.y);
+        assert_eq!(v.z, v32.z + v32.z);
+
+        // Subtraction
+        let v = v32 - v32;
+        assert_eq!(v.x, 0.0f32);
+
+        // Multiplication
+        let v = v32 * v32;
+        assert_eq!(v.x , v32.x * v32.x);
+
+        let mut v = v32.mulfed(3.0);
+        assert_eq!(v.x, v32.x * 3.0);
+
+        v.mulf(2.0);
+        assert_eq!(v.x, v32.x * 3.0 * 2.0);
     }
 
-    let v64 = Vector { x: 1.0f64, y: 2.0f64, z: 3.0f64, };
-    assert_eq!(v64.x, 1.0f64);
+    #[test]
+    fn normalize() {
+        let v = Vector { x: 2.0f32, y: 0.0, z: 0.0 };
+        assert_eq!(v.len(), 2.0);
+        assert_eq!(v.normalized().len(), 1.0);
 
-    println!("{:?}", v32);
-    println!("{:?}", v64);
+        let mut v = v;
+        assert_eq!(v.normalize().len(), 1.0);
 
-    // Addition
-    let v = v32 + v32;
-    assert_eq!(v.x, v32.x + v32.x);
-    assert_eq!(v.y, v32.y + v32.y);
-    assert_eq!(v.z, v32.z + v32.z);
-
-    let v = v32 - v32;
-    assert_eq!(v.x, 0.0f32);
-
-    let v = v32 * v32;
-    assert_eq!(v.x , v32.x * v32.x);
-
-    let mut v = v32.mulfed(3.0);
-    assert_eq!(v.x, v32.x * 3.0);
-
-    v.mulf(2.0);
-    assert_eq!(v.x, v32.x * 3.0 * 2.0);
+    }
 }
-
-// #[cfg(test)]
-// #[bench]
-// fn perf(b: &mut test::Bencher) {
-//     let v = Vector { x: 5.0f32, y: 4.0f32, z: 0.0f32 };
-//     let mut x = v;
-//     b.iter(|| {
-//         for _ in range(1, 1000000) {
-//             x = x + v;
-//         }
-//     })
-// }
