6a8a5c60f221b092cc5097bf28abd956c7857347
Sebastian Thiel <byronimo@gmail.com>
2015-02-09 15:33:43 +0100

    Added more options to allow setting all render attributes

--- src/rust/main.rs
+++ src/rust/main.rs
@@ -14,11 +14,19 @@ use std::old_path::Path;
 
 
 docopt!(Args derive Show, "
-Usage: rtrace (<OUTPUT-FILE>|-)
+Usage: rtrace [options] (<OUTPUT-FILE>|-)
        rtrace --help
 
+Options:
+--width <X>                    The width of the output image [default: 1024]
+--height <Y>                   The height of the output image [default: 1024]
+--samples-per-pixel <SAMPLES>  Amount of samples per pixel. 4 means 16 over-samples [default: 1]
+
 <OUTPUT-FILE>|-     Either a file with .tga extension, or - to write file to stdout
-");
+"
+, flag_samples_per_pixel: u16
+, flag_height: u16
+, flag_width: u16);
 
 
 #[allow(dead_code)]
@@ -45,9 +53,9 @@ fn main() {
         FileOrAnyWriter::AnyWriter(old_io::stdout())
     };
 
-    let options = RenderOptions {   width: 1024,
-                                    height: 1024,
-                                    samples_per_pixel: 1 };
+    let options = RenderOptions {   width: args.flag_width,
+                                    height: args.flag_height,
+                                    samples_per_pixel: args.flag_samples_per_pixel };
 
     Renderer::render(&options, s.clone(), 
                      &mut PPMStdoutRGBABufferWriter::new(false, &mut output), &pool);
